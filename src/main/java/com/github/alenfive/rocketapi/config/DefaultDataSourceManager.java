7ca033d4f7093dcc3afed68838e2e4c22beea08a
mihuajun <mihuajun@wangxiaobao.com>
2022-01-19 10:18:49 +0800

    1. 添加启动说明。2.添加magic-api快链

--- src/main/java/com/github/alenfive/rocketapi/config/DefaultDataSourceManager.java
+++ src/main/java/com/github/alenfive/rocketapi/config/DefaultDataSourceManager.java
@@ -4,6 +4,7 @@ import com.github.alenfive.rocketapi.datasource.DataSourceDialect;
 import com.github.alenfive.rocketapi.datasource.DataSourceManager;
 import com.github.alenfive.rocketapi.datasource.MongoDataSource;
 import com.github.alenfive.rocketapi.datasource.MySQLDataSource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
@@ -17,6 +18,7 @@ import java.util.Map;
  * 默认数据源管理器
  */
 @Component
+@Slf4j
 public class DefaultDataSourceManager extends DataSourceManager {
 
     @Autowired
@@ -27,6 +29,9 @@ public class DefaultDataSourceManager extends DataSourceManager {
 
     @PostConstruct
     public void init() {
+        log.warn("\n--------------------------------------\n" +
+                " !!! 不要启动我，我只为编译调试源码而生，调试成功后mvn package 会输出 'rocket-api-boot-starter',关于如何使用stater,请看业务集成demo:https://gitee.com/alenfive/rocket-api-demo" +
+                "\n--------------------------------------\n");
         Map<String,DataSourceDialect> dialectMap = new LinkedHashMap<>();
         dialectMap.put("mysql",new MySQLDataSource(dataSource,true));
         dialectMap.put("mongodb",new MongoDataSource(mongoTemplate));
